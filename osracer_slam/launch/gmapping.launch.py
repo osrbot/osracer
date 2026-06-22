ad855fa76f951398413ee6ef56c5065f3c2048ee
kitso666 <kitso@osrbot.com>
2026-06-22 05:42:08 -0400

    feat: Update SLAM algorithm parameters

--- osracer_slam/launch/gmapping.launch.py
+++ osracer_slam/launch/gmapping.launch.py
@@ -3,6 +3,7 @@ from launch import LaunchDescription
 from launch.substitutions import EnvironmentVariable
 import launch.actions
 import launch_ros.actions
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time', default='false')
@@ -12,11 +13,5 @@ def generate_launch_description():
             package='slam_gmapping', 
             executable = 'slam_gmapping',
             output = 'screen', 
-            parameters = [{
-                'use_sim_time':use_sim_time,
-                'base_frame': "base_footprint",
-                'map_frame': "map",
-                'odom_frame': "odom",
-            }])
-              
+            parameters=[os.path.join(get_package_share_directory("osracer_slam"), "param", "slam_gmapping.yaml")]),          
     ])
